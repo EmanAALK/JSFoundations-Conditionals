85c89e25dadeb7fae689ca16d6ef6878ca677d80
EmanAALK <emanalkandarii@gmail.com>
2020-06-15 20:31:43 +0300

    Task 2

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
 // Define the numbers and the operation here
-
+let num1 = 3;
+let num2 = 4;
+let op = `+`;
 // Write if-statements here
+
+if (op == `+`) {
+  console.log(num1 + num2);
+} else if (op == `-`) {
+  console.log(num1 - num2);
+} else if (op == `*`) {
+  console.log(num1 * num2);
+} else if (op == `/`) {
+  console.log(num1 / num2);
+} else {
+  console.log("Error");
+}
